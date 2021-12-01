8579f88c2ca8a4bc6443e08045ed1216670ed9dd
Eliel <elielmichel@gmail.com>
2021-12-01 18:25:30 -0300

    chore: parse file read

---
+++ lib/reports_generator.ex
@@ -0,0 +1,17 @@
+defmodule ReportsGenerator do
+  def build(filename) do
+    "reports/#{filename}"
+    |> File.stream!()
+    |> Enum.reduce(%{}, fn line, report ->
+      [id, _foodName, price] = parse_line(line)
+      Map.put(report, id, price)
+    end)
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(2, &String.to_integer/1)
+  end
+end
