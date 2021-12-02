3f9003308cce34896cbaf7cc114154d770368b2e
Eliel <elielmichel@gmail.com>
2021-12-02 19:21:31 -0300

    perf: create parallel tasks

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -26,6 +26,21 @@ defmodule ReportsGenerator do
     end)
   end
 
+  def build_from_many(filenames) when not is_list(filenames),
+    do: {:error, "Please provide a list"}
+
+  def build_from_many(filenames) do
+    result =
+      filenames
+      # fn filename -> build(filename end)
+      |> Task.async_stream(&build/1)
+      |> Enum.reduce(report_acc(), fn {:ok, result}, report ->
+        sum_reports(report, result)
+      end)
+
+    {:ok, result}
+  end
+
   # Captura o maior valor do map
   # Guards
   def fetch_higher_cost(report, option) when option in @options do
@@ -37,12 +52,25 @@ defmodule ReportsGenerator do
   def fetch_higher_cost(_report, option),
     do: {:error, "Unhandled option error: #{option} does not exist in type @foods"}
 
-  defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users} = report) do
+  defp sum_reports(%{"foods" => foods1, "users" => users1}, %{
+         "foods" => foods2,
+         "users" => users2
+       }) do
+    foods = merge_maps(foods1, foods2)
+    users = merge_maps(users1, users2)
+
+    build_report(foods, users)
+  end
+
+  defp merge_maps(map1, map2),
+    do: Map.merge(map1, map2, fn _key, value1, value2 -> value1 + value2 end)
+
+  defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users}) do
     users = Map.put(users, id, users[id] + price)
     foods = Map.put(foods, food_name, foods[food_name] + 1)
 
     # Pipe aqui serve para atualizar o Map
-    %{report | "users" => users, "foods" => foods}
+    build_report(foods, users)
   end
 
   # Pega uma coleção e converte em outra
@@ -50,6 +78,8 @@ defmodule ReportsGenerator do
     foods = Enum.into(@available_foods, %{}, &{&1, 0})
     users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 
-    %{"users" => users, "foods" => foods}
+    build_report(foods, users)
   end
+
+  defp build_report(foods, users), do: %{"users" => users, "foods" => foods}
 end
